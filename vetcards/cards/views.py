8505f0884bad04ca13cb9fa8657ad4743f71d646
droidroot1995 <droidroot1995@gmail.com>
2020-10-14 00:32:39 +0300

    or to and fix

--- vetcards/cards/views.py
+++ vetcards/cards/views.py
@@ -94,7 +94,7 @@ def vet_procs_list(request):
     pet = Pet.objects.filter(id=int(pid)).first()
     user = User.objects.filter(id=int(uid)).first()
     
-    if pet.user.id != uid or not user.vet:
+    if pet.user.id != uid and not user.vet:
         return JsonResponse({"errors": "you aren't a veterinar or owner of this pet"})
     
     procedures = Procedure.objects.filter(pet_id=int(pid)).values('pet_id', 'user_id', 'purpose', 'symptoms',
