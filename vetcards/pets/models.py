eef4a0a2550ebe1ba370b9d80cfb854767dbb18a
droidroot1995 <droidroot1995@gmail.com>
2020-10-20 23:48:24 +0300

    blank fields pet model

--- vetcards/pets/models.py
+++ vetcards/pets/models.py
@@ -6,11 +6,11 @@ class Pet(models.Model):
     user = models.ForeignKey(to='users.User', on_delete=models.CASCADE, verbose_name='Пользователь')
     name = models.TextField(max_length=64, null=True, verbose_name='Кличка')
     species = models.TextField(max_length=128, null=True, verbose_name='Вид')
-    breed = models.TextField(max_length=128, null=True, verbose_name='Порода')
-    color = models.TextField(max_length=128, null=True, verbose_name='Окрас')
-    birth_date = models.DateField(verbose_name='Дата рождения')
-    gender = models.TextField(max_length=10, null=True, verbose_name='Пол')
-    chip = models.TextField(max_length=64, null=True, verbose_name='Чип')
+    breed = models.TextField(max_length=128, null=True, blank=True, verbose_name='Порода')
+    color = models.TextField(max_length=128, null=True, blank=True, verbose_name='Окрас')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    gender = models.TextField(max_length=10, null=True, blank=True, verbose_name='Пол')
+    chip = models.TextField(max_length=64, null=True, blank=True, verbose_name='Чип')
     avatar = models.ImageField(upload_to='pavatars/', null=True, blank=True, verbose_name='Фотография')
     
     class Meta:
