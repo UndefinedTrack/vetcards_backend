aae6dc4619661b9540fb5d9196b777003acec241
droidroot1995 <droidroot1995@gmail.com>
2020-10-27 19:44:04 +0300

    fix: elasticsearch

--- vetcards/cards/documents.py
+++ vetcards/cards/documents.py
@@ -1,24 +1,30 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from .models import Procedure, OwnerProcedure
 
 @registry.register_document
 class OwnerProcedureDocument(Document):
+
+    pet_id = fields.IntegerField(attr='pet')
+    user_id = fields.IntegerField(attr='user')
     class Index:
         name = 'owner_procs'
         settings = {'number_of_shards': 1, 'number_of_replicas': 1}
         
     class Django:
         model = OwnerProcedure
-        fields = ['id', 'pet_id', 'user_id', 'name', 'description', 'proc_date']
+        fields = ['id', 'name', 'description', 'proc_date']
         
 @registry.register_document
 class VetProcedureDocument(Document):
+
+    pet_id = fields.IntegerField(attr='pet')
+    user_id = fields.IntegerField(attr='user')
     class Index:
         name = 'vet_procs'
         settings = {'number_of_shards': 1, 'number_of_replicas': 1}
         
     class Django:
         model = Procedure
-        fields = ['id', 'pet_id', 'user_id', 'purpose', 'symptoms',
-                  'diagnosis', 'recomms', 'recipe', 'proc_date']
+        fields = ['id', 'purpose', 'symptoms', 'diagnosis',
+                    'recomms', 'recipe', 'proc_date']
